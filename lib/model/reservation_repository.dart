a06c7cbcdd00ab2a1e6370138a85908461923aa1
Noah Joeris <noah.joeris@stud.h-da.de>
2021-06-20 20:48:27 +0200

    add methods in Reservation repo to get reservations

--- lib/model/reservation_repository.dart
+++ lib/model/reservation_repository.dart
@@ -6,4 +6,23 @@ class ReservationRepository {
   List<Reservation> get reservations => _reservations;
 
   ReservationRepository(this._reservations);
+
+  ///return: the upcoming reservations
+  List<Reservation> getUpcomingWorkspaceReservations(String workspaceID) {
+    return _reservations
+        .where((element) => element.dateTimeStart.isAfter(DateTime.now()))
+        .toList();
+  }
+
+  /// return: null if not booked or reservation if booked
+  Reservation? getCurrentWorkspaceReservation(String workspaceID) {
+    for (int i = 0; i < _reservations.length; i++) {
+      if (_reservations[i].workspaceID == workspaceID &&
+          _reservations[i].dateTimeStart.isBefore(DateTime.now()) &&
+          _reservations[i].dateTimeEnd.isAfter(DateTime.now())) {
+        return _reservations[i];
+      }
+    }
+    return null;
+  }
 }
